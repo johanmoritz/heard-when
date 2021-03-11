30273dd29e15b44a9dd79baf0db66d9b7256141e
Emma Olsson <emmaolss@kth.se>
2021-03-11 20:52:38 +0100

    Implemented comesBefore and started with guessOrder and correctGuess

--- src/game.ts
+++ src/game.ts
@@ -20,20 +20,10 @@ const nirvana: Card = {
   year: 1991
 };
 
-const maxCards: number = 10; // Should take input from host at setup phase.
+const maxCards = 10; // Should take input from host at setup phase.
 const emmasLockedCards: Card[] = [abba, nirvana];
 const emmasLooseCards: Card[] = [];
 const emmasCards: Card[] = emmasLockedCards.concat(emmasLooseCards);
-
-const compareYear = function(card1: Card, card2: Card) {
-  if (card1.year > card2.year) {
-    return -1;
-  }
-  if (card1.year < card2.year) {
-    return 1;
-  }
-  return 0;
-};
 
 const player1: Player = {
   id: 1,
@@ -43,23 +33,9 @@ const player1: Player = {
 };
 
 console.log(emmasCards);
-// player in turn guess on which index the card should be placed. Positions is a array of length cards (locked and loose concatinated).
-// this will either trigger correctGuess() the year is right in relation to each other (use compareYear() to check) or wrongGuess() if not.
-function guessOrder(hiddenCard: Card, positions: Card[cards.length+1], index: int) {
-  // if correct --> correctGuess()
-  // if wrong --> wrongGuess()
-}
-// this will be triggered in guessOrder() and will in turn place the card in the correct index in a temporary array.
-function correctGuess() {
-  // 1. place card in index + move other cards accordingly
-  // 2. trigger lockOrContinue() --> if lock --> lockCards() / else continue --> drawCard() 
-  // ... check if max cards reached --> victory!
-}
-// this will be triggered in guessOrder() when a player guess wrong.
-function wrongGuess() {
-  // 1. Check and remove potential looseCards.
-  // 2. Trigger yearGuess() for other players.
-  // 3. Update player in turn to the next player in turn.
+
+function comesBefore(card1: Card, card2: Card): boolean {
+  return card1.year < card2.year || card1.year === card2.year;
 }
 // this function is used to lock your loose cards from this round.
 function lockCards() {
@@ -68,4 +44,33 @@ function lockCards() {
 // this function will be triggered when player in turn guess wrong and allows other players to guess a specific year.
 function yearGuess() {
 
+}
+// this will be triggered in guessOrder() and will in turn place the card in the correct index in a temporary array.
+function correctGuess(positions: Card[], card: Card, index: number) {
+  // 1. place card in index + move other cards accordingly
+  positions.splice(index, 0, card);
+  positions.join;
+  // ... check if max cards reached --> victory!
+  if (positions.length === maxCards) {
+    console.log("VICTORY IS MINE!");
+  }
+  // 2. trigger lockOrContinue() --> if lock --> lockCards() / else continue --> drawCard().
+}
+// this will be triggered in guessOrder() when a player guess wrong.
+function wrongGuess() {
+  // 1. Check and remove potential looseCards.
+  // 2. Trigger yearGuess() for other players.
+  // 3. Update player in turn to the next player in turn.
+}
+// player in turn guess on which index the card should be placed. Positions is a array of length cards (locked and loose concatinated).
+// this will either trigger correctGuess() the year is right in relation to each other (use comesBefore() to check) or wrongGuess() if not.
+function guessOrder(hiddenCard: Card, positions: Card[], index: number) {
+  // if correct --> correctGuess()
+  if (comesBefore(hiddenCard, positions[index])) {
+    correctGuess(positions, hiddenCard, index);
+  }
+  // if wrong --> wrongGuess()
+  else {
+    wrongGuess();
+  }
 }
